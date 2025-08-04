0d2999d5f23314fed2a0b5af07d81a96cbb19f48
PROroblox1212 <alexischarlebois499@gmail.com>
2025-08-04 01:20:34 -0400

    Update casier.js

--- api/casier.js
+++ api/casier.js
@@ -1,1 +1,23 @@
+let casiers = [
+  { id: 1, nom: "Alex B", crime: "Vol de voiture", date: "2025-08-04" },
+  { id: 2, nom: "Marie C", crime: "Intrusion", date: "2025-08-01" }
+];
 
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    res.status(200).json(casiers);
+  } else if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk;
+    });
+    req.on('end', () => {
+      const nouveauCasier = JSON.parse(body);
+      nouveauCasier.id = casiers.length + 1;
+      casiers.push(nouveauCasier);
+      res.status(201).json({ success: true, casier: nouveauCasier });
+    });
+  } else {
+    res.status(405).end();
+  }
+}
